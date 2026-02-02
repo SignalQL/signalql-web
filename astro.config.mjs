86f2d58f1c7b6637bc6a004b10244f6b82530dee
Alex Bezverkhniy <alexandr.bezverkhniy@gmail.com>
2026-02-02 15:36:28 -0600

    added Docker and Nodejs standalon configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,19 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+import node from '@astrojs/node';
+
 export default defineConfig({
   site: 'https://signalql.io',
+
   markdown: {
     remarkPlugins: [],
     rehypePlugins: [],
   },
+
   integrations: [tailwind()],
+
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
